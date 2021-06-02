74f9e0855912cf43cd7c5270cb00a65f901138c5
Rwicheek Sarker <rn2osystems@gmail.com>
2021-06-02 12:37:42 +0600

    Add project files.

---
+++ AlgorithmServerTwo/Program.cs
@@ -0,0 +1,24 @@
+﻿using Common;
+using System;
+using System.ServiceModel;
+
+namespace AlgorithmServerTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(Compute)))
+            {
+                host.Open();
+                Console.WriteLine("Server is running.......");
+                Console.WriteLine("Type \"exit\" to shutdown");
+                string input = "";
+                while (input != "exit")
+                {
+                    input = Console.ReadLine();
+                }
+            }
+        }
+    }
+}
